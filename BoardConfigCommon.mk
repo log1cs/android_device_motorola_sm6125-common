390cc2646b184828c303ce068fa720de52cbad24
Tuan Anh <tuan73176@gmail.com>
2026-06-15 08:12:40 +0000

    sm6125-common: Inherit hardware/motorola SEPolicy
    Change-Id: Ie2d13f53af4fa4d65178ce5d172f1e41609ab962

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -46,6 +46,7 @@ TARGET_USERIMAGES_USE_F2FS := true
 
 # SEPolicy
 include device/qcom/sepolicy_vndr/SEPolicy.mk
+include hardware/motorola/sepolicy/qti/SEPolicy.mk
 
 # Treble
 TARGET_COPY_OUT_VENDOR := vendor
