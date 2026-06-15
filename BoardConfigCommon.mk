7eb17c2374ee5b988c5dc495a342d1aef77214ab
LuK1337 <priv.luk@gmail.com>
2026-06-15 08:16:02 +0000

    sm6125-common: Include common device matrices
    Change-Id: I86d8e202a8e48b9856c58d649925fee891bb26e4

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -30,6 +30,13 @@ AB_OTA_PARTITIONS += \
 TARGET_BOOTLOADER_BOARD_NAME := trinket
 TARGET_NO_BOOTLOADER := true
 
+# HIDL
+DEVICE_MATRIX_FILE += hardware/qcom-caf/common/compatibility_matrix.xml
+DEVICE_MANIFEST_FILE += hardware/qcom-caf/sm8150/media/conf_files/sm6150/c2_manifest.xml
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += \
+    hardware/qcom-caf/common/vendor_framework_compatibility_matrix.xml \
+    hardware/motorola/vintf/device_framework_matrix.xml
+
 # Partitions
 BOARD_FLASH_BLOCK_SIZE := 262144
 BOARD_BOOTIMAGE_PARTITION_SIZE := 67108864
