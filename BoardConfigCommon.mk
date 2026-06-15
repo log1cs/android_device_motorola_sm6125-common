9bc64e828ae9b9eda0a6ecd2307eec103138a01d
jmpfbmx <jmpf.bmx@gmail.com>
2026-06-15 08:10:39 +0000

    sm6125-common: Setup AVB
    Change-Id: I7d5385de553a8a1a54f6577c42252aff4974eb5f
    Signed-off-by: jmpfbmx <jmpf.bmx@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -50,5 +50,13 @@ include device/qcom/sepolicy_vndr/SEPolicy.mk
 # Treble
 TARGET_COPY_OUT_VENDOR := vendor
 
+# Verified Boot
+BOARD_AVB_ENABLE := true
+BOARD_AVB_MAKE_VBMETA_IMAGE_ARGS += --flags 3
+BOARD_AVB_RECOVERY_KEY_PATH := external/avb/test/data/testkey_rsa2048.pem
+BOARD_AVB_RECOVERY_ALGORITHM := SHA256_RSA2048
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX := 1
+BOARD_AVB_RECOVERY_ROLLBACK_INDEX_LOCATION := 1
+
 # Include the proprietary files BoardConfig.
 include vendor/motorola/sm6125-common/BoardConfigVendor.mk
