c36c7746c4b0ba73c0930fed8f2cd56e1a9e2d08
Tuan Anh <tuan73176@gmail.com>
2026-06-19 06:33:51 +0000

    sm6125-common: Import Wi-Fi configs
    sofiap_ao-user 12 S0PRS32M.44-11-10-26 86991 release-keys
    
    Change-Id: I34ad93769520607f5fe163790fb6b9f2332583d1

--- common.mk
+++ common.mk
@@ -64,6 +64,10 @@ PRODUCT_PACKAGES += \
     wpa_supplicant \
     wpa_supplicant.conf
 
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/wifi/wpa_supplicant_overlay.conf:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/wpa_supplicant_overlay.conf \
+    $(LOCAL_PATH)/configs/wifi/WCNSS_qcom_cfg.ini:$(TARGET_COPY_OUT_VENDOR)/etc/wifi/WCNSS_qcom_cfg.ini
+
 # WiFi firmware symlinks
 PRODUCT_PACKAGES += \
     firmware_WCNSS_qcom_cfg.ini_symlink \
