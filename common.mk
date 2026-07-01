925615ac52eeaf7d59f4c3d64470bfadb0d6af82
LuK1337 <priv.luk@gmail.com>
2026-07-01 04:25:47 +0000

    sm6125-common: Set AAPT config
    Change-Id: Ib4b5945d4296728cfb46045e812bd499b7f1920c

--- common.mk
+++ common.mk
@@ -23,6 +23,11 @@ PRODUCT_PACKAGES += \
     checkpoint_gc \
     otapreopt_script
 
+# AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREBUILT_DPI := xxhdpi xhdpi hdpi
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
 # Boot control
 PRODUCT_PACKAGES += \
     android.hardware.boot-service.qti \
