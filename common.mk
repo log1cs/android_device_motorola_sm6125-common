88ae2c73ad3b38527a5d87aed60a828727f1743f
Skyblueborb <tomaszborbely0710@gmail.com>
2026-07-01 04:30:52 +0000

    sm6125-common: Build USB HAL
    Change-Id: I32665bca59e4120d186edfd0e01c0d2eb47c4ad0

--- common.mk
+++ common.mk
@@ -94,7 +94,8 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/google/interfaces \
     hardware/google/pixel \
     hardware/lineage/interfaces/power-libperfmgr \
-    hardware/qcom-caf/common/libqti-perfd-client
+    hardware/qcom-caf/common/libqti-perfd-client \
+    vendor/qcom/opensource/usb/etc
 
 # Telephony
 PRODUCT_PACKAGES += \
@@ -114,6 +115,14 @@ PRODUCT_PACKAGES += \
     update_engine \
     update_engine_sideload \
     update_verifier
+
+# USB
+PRODUCT_PACKAGES += \
+    android.hardware.usb@1.3-service.dual_role_usb \
+    android.hardware.usb.gadget-service.qti \
+    init.qcom.usb.rc \
+    init.qcom.usb.sh \
+    usb_compositions.conf
 
 # WiFi
 PRODUCT_PACKAGES += \
