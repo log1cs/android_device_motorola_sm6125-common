570d25e1d70d9aaad04afa1633fa179620e0029d
LuK1337 <priv.luk@gmail.com>
2026-07-01 06:57:26 +0000

    sm6125-common: Build Lineage AIDL camera provider
    Change-Id: I290ecfd9760d6428fe64f137974c353846c0e35d

--- common.mk
+++ common.mk
@@ -47,6 +47,9 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.bluetooth_le.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.bluetooth_le.xml
 
 # Camera
+PRODUCT_PACKAGES += \
+    android.hardware.camera.provider-service_32.lineage
+
 PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.camera.flash-autofocus.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.flash-autofocus.xml \
     frameworks/native/data/etc/android.hardware.camera.front.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.camera.front.xml \
